4f7b5e37425b4e2597814d94b4a14c1dcbbc3b69
Yan Khamutouski <yan_kh28@mail.ru>
2018-04-09 22:51:33 +0300

    Created Database dependency, UI forms(sign in, sign up, launchScreen, tabBar).

--- tistic.co/tistic.co/AppDelegate.swift
+++ tistic.co/tistic.co/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //change the color of tab bar items
+        UITabBar.appearance().tintColor = UIColor(red:0.47, green:0.69, blue:0.44, alpha:1.0)
+        UITabBar.appearance().backgroundColor = UIColor(red:0.97, green:0.95, blue:0.95, alpha:1.0)
         return true
     }
 
